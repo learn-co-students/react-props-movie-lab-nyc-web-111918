ffb841980a03047ea352c07e87e1ed25b34e0b38
github username <github email address>
2019-01-28 20:51:12 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import MovieShowcase from './MovieShowcase';
+import MovieShowcase from './MovieShowcase.js';
 
 ReactDOM.render(<MovieShowcase />, document.getElementById('root'));
