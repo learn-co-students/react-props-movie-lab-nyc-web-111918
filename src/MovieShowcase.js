61fa7fc1e3d1e7c0e18054e6432241b8290d2a05
oahtpham <phamthaop.9@gmail.com>
2019-01-29 17:57:20 -0500

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import MovieCard from './card-components/MovieCard.js'
 import movieData from './data.js'
+import UUID from 'uuid';
 
 export default class MovieShowcase extends Component {
 
+
   generateMovieCards = () => {
-    // map over your movieData array and return the correct 
+    // map over your movieData array and return the correct
+    return movieData.map(movie => <MovieCard key={UUID()} {...movie}/>)
   }
 
   render() {
